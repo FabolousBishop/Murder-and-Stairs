151dad91de01c625aaa58eca7e6d4f1edffebc25
Bartosz Tomaszewski <72268678+FabolousBishop@users.noreply.github.com>
2021-11-04 11:47:57 +0100

    Add files via upload

---
+++ entity_factories.py
@@ -0,0 +1,111 @@
+from components.ai import HostileEnemy
+from components import consumable, equippable
+from components.equipment import Equipment
+from components.fighter import Fighter
+from components.inventory import Inventory
+from components.level import Level
+from entity import Actor, Item
+
+
+player = Actor(
+    char="@",
+    color=(255, 255, 255),
+    name="Player",
+    ai_cls=HostileEnemy,
+    equipment=Equipment(),
+    fighter=Fighter(hp=50, base_defense=1, base_power=2),
+    inventory=Inventory(capacity=26),
+    level=Level(level_up_base=200),
+)
+
+rat = Actor(
+    char="r",
+    color=(102, 51, 0),
+    name="Rat",
+    ai_cls=HostileEnemy,
+    equipment=Equipment(),
+    fighter=Fighter(hp=10, base_defense=0, base_power=3),
+    inventory=Inventory(capacity=0),
+    level=Level(xp_given=35),
+)
+goblin = Actor(
+    char="g",
+    color=(0, 127, 0),
+    name="Goblin",
+    ai_cls=HostileEnemy,
+    equipment=Equipment(),
+    fighter=Fighter(hp=16, base_defense=1, base_power=4),
+    inventory=Inventory(capacity=0),
+    level=Level(xp_given=50),
+)
+
+orc = Actor(
+    char="o",
+    color=(0, 127, 0),
+    name="Orc",
+    ai_cls=HostileEnemy,
+    equipment=Equipment(),
+    fighter=Fighter(hp=20, base_defense=2, base_power=5),
+    inventory=Inventory(capacity=0),
+    level=Level(xp_given=75),
+)
+
+troll = Actor(
+    char="T",
+    color=(0, 127, 0),
+    name="Troll",
+    ai_cls=HostileEnemy,
+    equipment=Equipment(),
+    fighter=Fighter(hp=24, base_defense=3, base_power=6),
+    inventory=Inventory(capacity=0),
+    level=Level(xp_given=125),
+)
+
+confusion_scroll = Item(
+    char="~",
+    color=(207, 63, 255),
+    name="Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
+fireball_scroll = Item(
+    char="~",
+    color=(255, 0, 0),
+    name="Fireball Scroll",
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
+)
+health_potion = Item(
+    char="!",
+    color=(127, 0, 255),
+    name="Health Potion",
+    consumable=consumable.HealingConsumable(amount=4),
+)
+big_health_potion = Item(
+    char="!",
+    color=(127, 0, 255),
+    name="Big Health Potion",
+    consumable=consumable.HealingConsumable(amount=10),
+)
+
+lightning_scroll = Item(
+    char="~",
+    color=(255, 255, 0),
+    name="Lightning Scroll",
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
+)
+
+dagger = Item(
+    char="/", color=(0, 191, 255), name="Dagger", equippable=equippable.Dagger()
+)
+
+sword = Item(char="/", color=(0, 191, 255), name="Sword", equippable=equippable.Sword())
+
+leather_armor = Item(
+    char="[",
+    color=(139, 69, 19),
+    name="Leather Armor",
+    equippable=equippable.LeatherArmor(),
+)
+
+chain_mail = Item(
+    char="[", color=(139, 69, 19), name="Chain Mail", equippable=equippable.ChainMail()
+)
